f00f24baa9e5c3c609e2ffabf6e31b37a6607017
VadimMaleev <maleeffset@yandex.by>
2023-08-17 15:25:22 +0300

    add delete all for SQL in testing all-data

--- src/api/public.api/testing/testing.controller.ts
+++ src/api/public.api/testing/testing.controller.ts
@@ -1,36 +1,21 @@
 import { Controller, Delete, HttpCode } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Post, PostDocument } from '../../../repositories/posts/posts.schema';
-import { Model } from 'mongoose';
-import {
-  Comment,
-  CommentDocument,
-} from '../../../repositories/comments/comments.shema';
-import { User, UserDocument } from '../../../repositories/users/users.schema';
-import { Blog, BlogDocument } from '../../../repositories/blogs/blogs.schema';
-import {
-  BannedUserForBlog,
-  BannedUsersForBlogDocument,
-} from '../../../repositories/users/banned.users.for.blog.schema';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Controller('testing')
 export class TestingController {
-  constructor(
-    @InjectModel(Post.name) private postModel: Model<PostDocument>,
-    @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
-    @InjectModel(User.name) private userModel: Model<UserDocument>,
-    @InjectModel(Blog.name) private blogModel: Model<BlogDocument>,
-    @InjectModel(BannedUserForBlog.name)
-    private bannedUserForBlogModel: Model<BannedUsersForBlogDocument>,
-  ) {}
+  constructor(@InjectDataSource() protected dataSource: DataSource) {}
   @Delete('all-data')
   @HttpCode(204)
   async deleteAll() {
-    await this.blogModel.deleteMany();
-    await this.postModel.deleteMany();
-    await this.commentModel.deleteMany();
-    await this.userModel.deleteMany();
-    await this.bannedUserForBlogModel.deleteMany();
+    await this.dataSource.query(
+      `
+      DELETE FROM public."Devices"
+      DELETE FROM public."Users"
+      DELETE FROM public."RecoveryCodes"
+      DELETE FROM public."Tokens"
+      `,
+    );
     return true;
   }
 }
